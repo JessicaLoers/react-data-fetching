9289480f4380bb6496bcf3ca2a739d5b73ff5544
Jessica Lörs <jessica.loers@gmail.com>
2024-01-17 23:33:02 +0100

    add funny joke toggle

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -1,33 +1,50 @@
-// Importing SWRConfig from SWR, which is used to set global configurations for SWR in your application
 import useSWR from "swr";
 import { useState } from "react";
 
 export default function Joke() {
+  const [jokesInfo, setJokesInfo] = useState([]);
   const [id, setId] = useState(0);
 
-  // Using the useSWR hook to fetch data
-  // The URL depends on the current "id", so a new joke is fetched every time "id" changes
-  // The hook returns an object containing the fetched data, loading state and error state
   const {
-    data: joke, // "data" is being renamed to "joke" for clarity
-    isLoading, // boolean to indicate if the request is still loading
-    error, // object containing error information if the request fails
+    data: joke,
+    isLoading,
+    error,
   } = useSWR(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
 
-  // If there's an error in fetching data, display an error message
   if (error) {
     return <h1>failed to load: {error.message}</h1>;
   }
-  // If data is still loading, display a loading message.
+
   if (isLoading) {
     return <h1> 🤡 Loading...</h1>;
   }
+
+  function handleToggleFunny(id) {
+    setJokesInfo((prevJokes) => {
+      const foundJoke = prevJokes.find((joke) => joke.id === id);
+
+      if (foundJoke) {
+        return prevJokes.map((joke) =>
+          joke.id === id ? { ...joke, isFunny: !joke.isFunny } : joke
+        );
+      }
+
+      return [...prevJokes, { id, isFunny: true }];
+    });
+  }
+
+  const { isFunny } = jokesInfo.find((info) => info.id === id) ?? {
+    isFunny: false,
+  };
 
   return (
     <>
       <small>ID: {id}</small>
       <h1>{joke.joke}</h1>
       <div>
+        <button type="button" onClick={() => handleToggleFunny(id)}>
+          {isFunny ? "stop laughing" : "start laughing"}
+        </button>
         <button type="button" onClick={() => setId(joke.prevId)}>
           ← Prev Joke
         </button>
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,12 @@
-// Importing SWRConfig from SWR, which is used to set global configurations for SWR in your application.
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 
-// Defining the "fetcher" function, which is used by SWR to know how to fetch data
-// It's using the browser's "fetch" API to make a network request, then parsing the response as JSON
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      {/* SWRConfig component from SWR library is used here to set global configurations.
-          - "value" prop receives an object with configuration options and the "fetcher" function from above is passed.
-          - ➡️ This means that every use of SWR in this application will use this "fetcher" function
-            by default to make network requests. */}
       <SWRConfig value={{ fetcher }}>
         <Component {...pageProps} />
       </SWRConfig>
